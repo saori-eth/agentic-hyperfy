ccb89f61e3b6c2e5d5db9edca1fd5c8c21b9efa0
saori-eth <d84da@proton.me>
2026-01-13 16:35:57 -0600

    reset example app

--- apps/spinning-box/index.js
+++ apps/spinning-box/index.js
@@ -1,20 +1,11 @@
 /**
- * WebView Test
+ * Spinning Box
  *
- * Tests the WebView node with TradingView widget
+ * Edit this file and save to hot-reload!
  */
 
-// Hide the default placeholder block
-app.get('Block').visible = false
+import { rotateApp } from './helper.js'
 
-// Create a webview with TradingView widget
-const webview = app.create('webview', {
-  // add rick roll iframe youtube link
-  src: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-  width: 3.2,
-  height: 1.8,
-  position: [0, 1.5, 0],
-  factor: 200, // Higher resolution for charts
+app.on('update', delta => {
+  rotateApp(delta)
 })
-
-app.add(webview)
